e7498736728c4b9809633d66c58c2c379f7de9dd
Bishal Kumar <Bishalk@attosol.com>
2023-12-22 15:30:37 +0530

    first commit

---
+++ constants/EducationData.js
@@ -0,0 +1,25 @@
+export const EducationData = [
+  {
+    name: "Bachelor of Technology (CSE)",
+    image:
+      "https://images.shiksha.com/mediadata/images/1618476358phpdlIFKQ.jpeg",
+    schoolOrCollege: "Assam Science and Technology University",
+    fromTo: "2014 - 2021",
+    statusOrPrecentage: "58.4%",
+  },
+  {
+    name: "Senior Secondary School (CBSE)",
+    image: "https://www.spsghy.co.in/images/spsresources/schoolbuilding1.jpg",
+    schoolOrCollege: "South Point School, Guwahati",
+    fromTo: "2012-2014",
+    statusOrPrecentage: "67%",
+  },
+  {
+    name: "Primary/Secondary School (CBSE)",
+    image:
+      "https://sfsguwahati.edu.in/wp-content/uploads/2021/04/sfs002-2048x1362.jpg",
+    schoolOrCollege: "Saint Francis D'Assisi Senior Secondary School, Guwahati",
+    fromTo: "2012",
+    statusOrPrecentage: "74%",
+  },
+];
---
+++ constants/SocialMediaData.js
@@ -0,0 +1,34 @@
+import { AiOutlineInstagram } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import { FaHackerrank } from "react-icons/fa";
+
+export const SocialMediaData = [
+  {
+    icon: <AiOutlineInstagram />,
+    color:
+      "linear-gradient(135deg, #833AB4, #C13584, #E1306C, #FD1D1D, #F56040, #FFDC80)",
+    link: "https://www.instagram.com/bishalkumar.war",
+  },
+  {
+    icon: <IoClose />,
+    color: "#0f0f0f",
+    link: "https://twitter.com/Vishal_kumar_01",
+  },
+  {
+    icon: <FaHackerrank />,
+    color: "#0f0f0f",
+    link: "https://www.hackerrank.com/profile/bishalkumar_war",
+  },
+  {
+    icon: <FaLinkedinIn />,
+    color: "#0072b1",
+    link: "https://www.linkedin.com/in/bishal-kumar-832398158/",
+  },
+  {
+    icon: <BsGithub />,
+    color: "#171515",
+    link: "https://github.com/blast0",
+  },
+];
