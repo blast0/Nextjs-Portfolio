c1e99fbdf84442437a75606d695f20dbc5e1bc7b
Bishal Kumar <Bishalk@attosol.com>
2023-12-26 20:00:30 +0530

    12

--- layout/navbar/NavbarItems.js
+++ layout/navbar/NavbarItems.js
@@ -1,26 +1,34 @@
 export const NavbarMenu = [
-    {
-        name: "Home",
-        link: "#home",
-    },
-    {
-        name: "About",
-        link: "#about",
-    },
-    {
-        name: "Tech Stack",
-        link: "#techStack",
-    },
-    {
-        name: "Education",
-        link: "#education",
-    },
-    {
-        name: "Experience",
-        link: "#experience",
-    },
-    {
-        name: "Project",
-        link: "#project",
-    },
+  {
+    name: "Home",
+    link: "#home",
+  },
+  {
+    name: "About",
+    link: "#about",
+  },
+  {
+    name: "Tech Stack",
+    link: "#techStack",
+  },
+  {
+    name: "Education",
+    link: "#education",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  //   {
+  //     name: "Project",
+  //     link: "#project",
+  //   },
+  {
+    name: "Blogs",
+    link: "#blogs",
+  },
+  {
+    name: "Apps",
+    link: "#apps",
+  },
 ];
