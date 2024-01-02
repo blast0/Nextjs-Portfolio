c0b8557d695e3177fc9879870ea333b3cee955bb
Bishal Kumar <Bishalk@attosol.com>
2024-01-02 16:08:54 +0530

    exp desc alignment

--- constants/ExperienceData.jsx
+++ constants/ExperienceData.jsx
@@ -5,20 +5,20 @@ export const ExperienceData = [
     role: "Software Engineer",
     fromTo: "September 2022 - Present",
     description: `-Improved page loading speed by 5-6X
-    -Improved Component Animation Speed from heavy lag to smooth 
-    -Converted Large Class Based Components to Functional Components as per requirements.
-    -Implemented Several Tools for our Product
-     --A stylish QR-generator
-     --A Cover Image/Hero Image/SVG Maker
-     --A GIF Maker
-     --A Dynamic TextEditor for writing Hugo Shortcodes
-    and many many more...
-    Languages used: HTML | CSS | SCSS | LESS | JAVASCRIPT 
-    Frameworks/Libraries/Runtime Environment: REACT | NEXTJS | REACT-NATIVE | NODEJS | HUGO | REDUX
-    Database: MongoDB
-    Cloud: Microsofr Azure
-    Version Control: GIT | SMART-GIT
-    Other Development Environments: Power App, Power Automate, Power BI`,
+-Improved Component Animation Speed from heavy lag to smooth 
+-Converted Large Class Based Components to Functional Components as per requirements.
+-Implemented Several Tools for our Product
+ --A stylish QR-generator
+ --A Cover Image/Hero Image/SVG Maker
+ --A GIF Maker
+ --A Dynamic TextEditor for writing Hugo Shortcodes
+ and many many more...
+Languages used: HTML | CSS | SCSS | LESS | JAVASCRIPT 
+Frameworks/Libraries/Runtime Environment: REACT | NEXTJS | REACT-NATIVE | NODEJS | HUGO | REDUX
+Database: MongoDB
+Cloud: Microsofr Azure
+Version Control: GIT | SMART-GIT
+Other Development Environments: Power App, Power Automate, Power BI`,
     side: "left",
   },
   {
@@ -27,7 +27,8 @@ export const ExperienceData = [
     role: "IT Supervisor",
     fromTo: "July 2019 - Nov 2021",
     description: `-Online Digital Examination-Installation of essential softwares, maintaining network security.
--Deploying data leakage protection, Debugging software & hardware related issues, Remote Proctoring of online exams. 
+-Deploying data leakage protection, Debugging software & hardware related issues,
+-Remote Proctoring of online exams. 
 -Handling servers and client computers,
 -Uploading and Downloading Sensitive DATA to and from the FTP Servers, provide remote debugging assistance to teams working on other locations Online.
 -Digital Examination-Installation of essential softwares, maintaining network security, Deploying data leakage protection. 
